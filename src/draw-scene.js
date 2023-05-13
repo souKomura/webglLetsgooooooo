763ac7ba0949f2533305d0cf2c595c5d3d7f5845
sou <shuhei.7018@gmail.com>
2023-05-13 19:28:48 +0900

    1st

---
+++ src/draw-scene.js
@@ -0,0 +1,32 @@
+function drawScene(gl, programInfo, buffers){
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const proj = mat4.create();
+    mat4.perspective(proj, Math.PI/4.0, gl.canvas.clientWidth/gl.canvas.clientHeight, 0.1, 100.0);
+
+    const model = mat4.create();
+
+    mat4.translate(model, model, [0.0, 0.0, -6.0]);
+
+    serPositionAttribute(gl, buffers, programInfo);
+
+    gl.useProgram(programInfo.program);
+
+    gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, proj);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.modelMatrix, false, model);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+function serPositionAttribute(gl, buffers, programInfo){
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, 2, gl.FLOAT, false, 0, 2);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+}
+
+export {drawScene};
---
+++ src/main.js
@@ -0,0 +1,79 @@
+
+import {initBuffers} from "./init-buffers.js";
+import {drawScene} from "./draw-scene.js";
+import { mat4 } from "gl-matrix";
+
+function loadShader(gl, type, source){
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+        alert(`failed compile shader: ${gl.getShaderInfoLog(shader)}`);
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+function initShaderProgram(gl, vsSource, fsSource){
+    const vert = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const frag = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vert);
+    gl.attachShader(shaderProgram, frag);
+
+    gl.linkProgram(shaderProgram);
+
+    if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
+        alert(`failed initialize shader program ${gl.getProgramInfoLog(shaderProgram)}`);
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+function main(){
+    const canvas = document.querySelector("#glCanvas");
+    const gl = canvas.getContext("webgl");
+
+    if(!gl){
+        alert("failed initialize webGL");
+        return;
+    }
+
+    const vsSource = `
+        attribute vec4 aPos;
+        uniform mat4 model;
+        uniform mat4 proj;
+        void main() {
+            gl_Position = proj * model * aPos;
+        }
+    `;
+
+    const fsSource = `
+        void main() {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+    `;
+
+    const shader = initShaderProgram(gl, vsSource, fsSource);
+
+    const programInfo = {
+        program: shader,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shader, "aPos"),
+        },
+        uniformLocations: {
+            modelMatrix: gl.getUniformLocation(shader, "model"),
+            projectionMatrix: gl.getUniformLocation(shader, "proj")
+        }
+    }
+
+    const buffers = initBuffers(gl);
+    drawScene(gl, programInfo, buffers);
+}
+
+window.onload = main;
